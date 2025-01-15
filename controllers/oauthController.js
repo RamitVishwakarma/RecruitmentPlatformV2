260ebc9e5188c7a27b449d7c3e6fbe447eee90ca
AbhinavGupta <bengupta786@gmail.com>
2025-01-15 23:14:58 +0530

    fixed/random-password-and-photo & feat/multi-account-login

--- controllers/oauthController.js
+++ controllers/oauthController.js
@@ -1,4 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const loginSuccess = asyncHandler((req, res) => {
   if (!req.user) {
@@ -13,9 +16,19 @@ const loginSuccess = asyncHandler((req, res) => {
 const logout = asyncHandler((req, res) => {
   req.logout((err) => {
     if (err) return res.status(500).json({ error: "Logout Failed" });
-    res.clearCookie("connect.sid");
-    const googleLogoutURL = "https://accounts.google.com/logout";
-    res.redirect(googleLogoutURL);
+    req.session.destroy(() => {
+      res.clearCookie("connect.sid");
+      res.redirect(
+        "https://accounts.google.com/o/oauth2/v2/auth" +
+          "?response_type=code" +
+          "&client_id=" +
+          process.env.GOOGLE_CLIENT_ID +
+          "&redirect_uri=" +
+          encodeURIComponent(process.env.CALLBACK_URL) +
+          "&scope=profile%20email" +
+          "&prompt=select_account",
+      );
+    });
   });
 });
 
--- utils/passport.js
+++ utils/passport.js
@@ -1,7 +1,10 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import prisma from "../utils/prisma.js";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 passport.use(
@@ -10,23 +13,35 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.CALLBACK_URL,
+      passReqToCallback: true,
+      scope: ["profile", "email"],
+      prompt: "select_account",
     },
-    async (accessToken, refreshToken, profile, done) => {
+    async (req, accessToken, refreshToken, profile, done) => {
       try {
         let user = await prisma.user.findUnique({
           where: { googleId: profile.id },
+          select: { id: true, name: true, email: true, photo: true },
         });
+        if (user) {
+          await prisma.user.update({
+            where: { googleId: profile.id },
+            data: { photo: profile.photos[0]?.value },
+          });
+        } else {
+          const randomPassword = crypto.randomBytes(16).toString("hex");
+          const hashedPassword = await bcrypt.hash(randomPassword, 10);
 
-        if (!user) {
           user = await prisma.user.create({
             data: {
               googleId: profile.id,
               email: profile.emails[0].value,
               name: profile.displayName,
+              photo: profile.photos[0]?.value || null,
+              password: hashedPassword,
             },
           });
         }
-
         return done(null, user);
       } catch (err) {
         return done(err, null);
@@ -43,6 +58,7 @@ passport.deserializeUser(async (id, done) => {
   try {
     const user = await prisma.user.findUnique({
       where: { id },
+      select: { id: true, name: true, email: true, photo: true },
     });
     done(null, user);
   } catch (err) {
