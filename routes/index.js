76d33f1dc917b2903cd5831f74246f062874eb9a
Yashvardhan Singh <yashvardhansingh112233@gmail.com>
2025-03-05 19:27:26 +0530

    feat/added-update-user-details

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import authRoutes from "./authRoutes.js";
 import socialRoutes from "./socialRoutes.js";
 import oauthRoutes from "./oauthRoutes.js";
 import adminRoutes from "./adminRoutes.js";
+import userDetailsUpdateRoutes from "./userDetailsUpdateRoutes.js";
 
 const router = express.Router();
 
@@ -10,5 +11,12 @@ router.use("/", authRoutes);
 router.use("/", oauthRoutes);
 router.use("/", socialRoutes);
 router.use("/", adminRoutes);
+router.use("/", userDetailsUpdateRoutes);
 
-export { authRoutes, socialRoutes, oauthRoutes, adminRoutes };
+export {
+  authRoutes,
+  socialRoutes,
+  oauthRoutes,
+  adminRoutes,
+  userDetailsUpdateRoutes,
+};
