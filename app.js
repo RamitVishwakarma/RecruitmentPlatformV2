05c4966f033b00d5093a7910ef7c1fb57db290fb
Ramit Vishwakarma <vishwakarmaramit@gmail.com>
2025-04-12 01:42:46 +0530

    Update .env.example and app.js for improved configuration and request handling

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ app.get("/", (req, res) => {
   );
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
